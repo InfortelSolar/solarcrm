9fcb3f92809276788faf4e67c302fc3d88ef4199
InfortelSolar <infortelsolaradm@gmail.com>
2026-05-15 09:52:10 -0300

    Update gdash.js

--- solarcrm/api/gdash.js
+++ solarcrm/api/gdash.js
@@ -1,1 +1,8 @@
-
+export default async function handler(req, res) {
+  const response = await fetch(
+    'https://public-api.gdash.io/api/v1/solar/plants?apikey=3HnfW02lkFxhrG92G9Ek8'
+  );
+  const data = await response.json();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(data);
+}
